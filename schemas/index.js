d4a6890473142572a565acd899479456b4d250af
Arya_05 <arya.madan@somaiya.edu>
2023-07-07 22:16:12 +0530

    added new files

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import user from "./user"
+import save from "./save"
+import pix from "./pix"
+import postedBy from "./postedBy"
+import comment from "./comment"
+export const schemaTypes = [user,save,pix,postedBy,comment];
